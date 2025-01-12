df58553474032833839a0a1f60188907cbd1b556
najasoft <nadiri@najasoft.com>
2025-01-12 21:32:39 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AdminComponent } from './pages/admin/admin.component';
 import { StationsComponent } from './pages/stations/stations.component';
 import { VelosComponent } from './pages/velos/velos.component';
@@ -17,8 +19,6 @@ import { LogoutComponent } from './pages/auth/logout/logout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { StationDetailsComponent } from './pages/station-details/station-details.component';
 import { BikeDetailsComponent } from './pages/bike-details/bike-details.component';
-import { AuthInterceptorService } from './services/auth-interceptor.service';
-import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,8 +29,8 @@ import { CommonModule } from '@angular/common';
     ReservationsComponent,
     UsersComponent,
     NavbarComponent,
-    LoginComponent,
     RegisterComponent,
+    LoginComponent,
     LogoutComponent,
     HomeComponent,
     StationDetailsComponent,
@@ -41,14 +41,9 @@ import { CommonModule } from '@angular/common';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
-    CommonModule
+    ReactiveFormsModule
   ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true,
-},],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
